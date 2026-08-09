8a93bf3d73eca3d4e7c7ecfc883257043a890953
htikkke <htikew574@gmail.com>
2026-08-09 14:30:06 +0630

    add js for toggle menu.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const menuIcon = document.getElementById("menu-icon");
+const closeIcon = document.getElementById("close-icon");
+const mainContainer = document.getElementById("main-container");
+const menuContainer = document.getElementById("menu-container");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
+  mainContainer.classList.add("hidden");
+  menuContainer.classList.remove("hidden");
+});
+
+closeIcon.addEventListener("click", () => {
+  closeIcon.classList.add("hidden");
+  menuIcon.classList.remove("hidden");
+  mainContainer.classList.remove("hidden");
+});
